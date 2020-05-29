a39500be0801b6829dfe15fd5a0d54eb3ff4362f
Mior Muhammad Zaki <crynobone@gmail.com>
2020-05-29 17:47:18 +0800

    Update release.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Feature/ReleaseTest.php
+++ tests/Feature/ReleaseTest.php
@@ -2,12 +2,45 @@
 
 namespace Satifest\Foundation\Tests\Feature;
 
+use Carbon\Carbon;
+use Carbon\CarbonInterface;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Satifest\Foundation\Release;
 use Satifest\Foundation\Tests\TestCase;
 
 class ReleaseTest extends TestCase
 {
+    /** @test */
+    public function it_can_cast_published_at_to_carbon()
+    {
+        $release = \factory(Release::class)->make([
+            'published_at' => $now = Carbon::now(),
+        ]);
+
+        $publishedAt = $release->published_at;
+
+        $this->assertInstanceOf(CarbonInterface::class, $publishedAt);
+        $this->assertSame($now->toDateTimeString(), $publishedAt->toDateTimeString());
+    }
+
+    /** @test */
+    public function it_can_get_name_attribute()
+    {
+        $release = \factory(Release::class)->make([
+            'title' => 'Version 1',
+            'version' => 'v1.0.0',
+        ]);
+
+        $this->assertSame('Version 1', $release->name);
+
+        $release = \factory(Release::class)->make([
+            'title' => null,
+            'version' => 'v1.0.0',
+        ]);
+
+        $this->assertSame('v1.0.0', $release->name);
+    }
+
     /** @test */
     public function it_belongs_to_repository_relation()
     {
