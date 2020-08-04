004bd88b3368745dbb332fa509cd5d98a1600f9e
Mior Muhammad Zaki <crynobone@gmail.com>
2020-08-04 13:50:38 +0800

    $user->createLicense($license, '*'); will assign all plans to current license.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Concerns/Licensable.php
+++ src/Concerns/Licensable.php
@@ -5,6 +5,7 @@ namespace Satifest\Foundation\Concerns;
 use Illuminate\Database\Eloquent\Collection;
 use Satifest\Foundation\Contracts\Licensing;
 use Satifest\Foundation\License;
+use Satifest\Foundation\Plan;
 
 trait Licensable
 {
@@ -21,7 +22,7 @@ trait Licensable
     /**
      * Create a new license for the user.
      *
-     * @param  \Illuminate\Support\Collection|array  $plans
+     * @param  \Illuminate\Support\Collection|array|string  $plans
      */
     public function createLicense(Licensing $licensing, $plans = []): License
     {
@@ -34,6 +35,10 @@ trait Licensable
             'currency' => (string) $licensing->price()->getCurrency(),
             'ends_at' => $licensing->endsAt(),
         ]);
+
+        if ($plans === '*') {
+            $plans = Plan::pluck('id')->all();
+        }
 
         if (($plans instanceof Collection && $plans->isNotEmpty())
             || (\is_array($plans) && ! empty($plans))
