46fd9bfca4ffec5344df5d9cb78ac2708588d160
Mior Muhammad Zaki <crynobone@gmail.com>
2020-08-06 08:14:09 +0800

    Use api_token for now.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Satifest.php
+++ src/Satifest.php
@@ -41,7 +41,7 @@ class Satifest
      *
      * @var string
      */
-    protected static $authTokenName = 'auth_token';
+    protected static $authTokenName = 'api_token';
 
     /**
      * Set purchaser model.
